861de2528756dae6b55716a0ed3bf67e37a8704c
eugenbejenar <jekabejenar@gmail.com>
2019-11-02 11:01:54 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+var myCurrentAge = 25;
+var result = (myCurrentAge >= 25) ? "I am over 25 yers" : "I am under 25 yers";
+console.log(result);
+
+var myAge = 25;
+if (myAge >= 25) { console.log('I am over 25 yers'); } else { console.log('I am under 25 yers'); }
