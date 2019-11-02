7af5df06a2da13f93d25f447d779fcc20e769ff2
eugenbejenar <jekabejenar@gmail.com>
2019-11-02 12:56:48 +0200

    hw10

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ function validateForm() {
     var x = document.forms["myForm"]["fname"].value;
     if (x == "") {
         alert("Your age must be filled out");
-    } else if (x >= 25) {
+    } else if (x > 25) {
         document.write('I am over 25 years old');
+
+    } else if (x == 25) {
+        document.write('I am 25');
     } else {
-        document.write('I am under 25')
+        document.write('I am under 25');
 
     }
 }
